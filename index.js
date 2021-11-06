6f6a2fdb0c89528fd15a65e5715f1219acd313dc
intisar eusha <iftikharrasha@gmail.com>
2021-11-07 04:32:58 +0600

    offer details added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const port = 5000;
 
 const app = express();
@@ -11,56 +12,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}@cluster0.ce7h0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-
-//   const offers = {
-//         category: "Camping",
-//         title: "Experience an island night at a variety of resorts",
-//         img: "https://cookislands.travel/sites/default/files/styles/max_1300x1300/public/tumunu_ttd-islandNights-A23-2.jpg?itok=JfH5_q0j",
-//         desc: "No visit is complete without an ‘Island Night’ cultural show for an unforgettable experience. Available at a variety of resorts and cultural centres on Rarotonga, Aitutaki and Atiu, they are a wonderful evening where passionate locals share our history and traditions through music, song, dance and food. Highly entertaining and fun for everyone, especially when the crowd participates in our traditional dance moves. A highlight for visitors are the exhilarating performances at cultural villages like Te Vara Nui and Highland Paradise Culture Centre or by local dance teams during the island nights staged around Rarotonga by the resorts and hotels. Delicious buffet meals featuring favourite local dishes like ika mata (marinated raw fish salad) are included in the island night packages. There are also the island nights where an umu kai is featured. This buffet style feast of food cooked in a traditional earth oven or umu is a wonderful experience.",
-//         price: 180
-//   };
-
-//   collection.insertOne(offers).then(() => {
-//       console.log('offer inserted!')
-//   })
-// });
-
-
 async function run() {
     try {
-      await client.connect();
-      const database = client.db("caymanDb");
-      const offersCollection = database.collection("offers");
+        await client.connect();
+        const database = client.db("caymanDb");
+        const offersCollection = database.collection("offers");
 
-      //Get Api
-      app.get('/all-offers', async (req, res) => {
-          const cursor = offersCollection.find({});
-          const offers = await cursor.toArray();
-          res.send(offers);
-      })
+        //Get Api for offers
+        app.get('/all-offers', async (req, res) => {
+            const cursor = offersCollection.find({});
+            const offers = await cursor.toArray();
+            res.send(offers);
+        })
 
-      //Post Api
-      app.post('/add-offers', async(req, res) => {
-            const offer = req.body; //console.log(req.body);
-            const result = await offersCollection.insertOne(offer);
+        //Single Get Api for offers
+        app.get('/place-order/:orderId', async (req, res) => {
+            const orderId = req.params.orderId;
+            const query = { _id: ObjectId(orderId) };
+            const offer = await offersCollection.findOne(query);
+            res.send(offer);
+        })
 
-            console.log('A document was inserted:', result);
-            res.json(result); //output on client site as a json
-      })
+        //Post Api for offers
+        app.post('/add-offers', async(req, res) => {
+                const offer = req.body; //console.log(req.body);
+                const result = await offersCollection.insertOne(offer);
 
-    // create a document to insert
-    //   const offers = {
-    //         category: "Camping",
-    //         title: "Experience an island night at a variety of resorts",
-    //         img: "https://cookislands.travel/sites/default/files/styles/max_1300x1300/public/tumunu_ttd-islandNights-A23-2.jpg?itok=JfH5_q0j",
-    //         desc: "No visit is complete without an ‘Island Night’ cultural show for an unforgettable experience. Available at a variety of resorts and cultural centres on Rarotonga, Aitutaki and Atiu, they are a wonderful evening where passionate locals share our history and traditions through music, song, dance and food. Highly entertaining and fun for everyone, especially when the crowd participates in our traditional dance moves. A highlight for visitors are the exhilarating performances at cultural villages like Te Vara Nui and Highland Paradise Culture Centre or by local dance teams during the island nights staged around Rarotonga by the resorts and hotels. Delicious buffet meals featuring favourite local dishes like ika mata (marinated raw fish salad) are included in the island night packages. There are also the island nights where an umu kai is featured. This buffet style feast of food cooked in a traditional earth oven or umu is a wonderful experience.",
-    //         price: 180
-    //     };
-    //   const result = await offersCollection.insertOne(offers);
-    //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+                console.log('A document was inserted:', result);
+                res.json(result); //output on client site as a json
+        })
+
     } finally {
     //   await client.close();
     }
