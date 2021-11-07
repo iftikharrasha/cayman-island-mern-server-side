475d4dc8252a8203cc61ee71eeef5d4e22c07ada
intisar eusha <iftikharrasha@gmail.com>
2021-11-07 19:58:42 +0600

    proceeding to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors()); //middleware
